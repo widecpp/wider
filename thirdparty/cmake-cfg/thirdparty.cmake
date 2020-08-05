d09a091b7b7652ea40f32a26d90b37f58a213c3a
Серый Волк <4591995+faserg1@users.noreply.github.com>
2020-08-05 21:27:15 +0300

    Testing something

--- thirdparty/cmake-cfg/thirdparty.cmake
+++ thirdparty/cmake-cfg/thirdparty.cmake
@@ -1,5 +1,8 @@
+set(CMAKE_MODULE_PATH ${CMAKE_MODULE_PATH} "${CMAKE_SOURCE_DIR}/cmake/")
+
 find_package(SDL2 CONFIG REQUIRED)
 find_package(imgui CONFIG REQUIRED)
+
 find_package(OpenGL CONFIG REQUIRED)
 
 find_path(BOOST_DLL_INCLUDE_DIRS "boost/dll.hpp")
