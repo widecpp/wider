5eb6072828b31d1c79a5dfad1cbe30d4cbaecde3
Серый Волк <faserg1@users.noreply.github.com>
2020-08-14 13:27:55 +0300

    Testing hax #define import

--- src/api/loader/loader.cpp
+++ src/api/loader/loader.cpp
@@ -1,5 +1,11 @@
 ﻿#include "loader.hpp"
+#ifndef _MSC_VER
+#define import import__boost_dll
+#endif // _MSC_VER
 #include <boost/dll.hpp>
+#ifndef _MSC_VER
+#undef import // import__boost_dll
+#endif // _MSC_VER
 
 using namespace wider::api::loader;
 
