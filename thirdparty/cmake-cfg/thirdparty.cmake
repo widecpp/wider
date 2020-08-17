fedfc3ef5d9f5935a1c693012654ff978b2a8510
faserg1 <dev@hrookami.icu>
2020-08-17 16:16:03 +0300

    Install file watcher

--- thirdparty/cmake-cfg/thirdparty.cmake
+++ thirdparty/cmake-cfg/thirdparty.cmake
@@ -4,4 +4,6 @@ find_package(imgui CONFIG REQUIRED)
 find_package(OpenGL REQUIRED)
 find_package(glad CONFIG REQUIRED)
 
+#find_package(efsw CONFIG REQUIRED)
+
 find_path(BOOST_DLL_INCLUDE_DIRS "boost/dll.hpp")
