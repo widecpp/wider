4d8d0aaae725debe17b22852e421a6a7723ea585
Серый Волк <faserg1@users.noreply.github.com>
2020-09-10 11:13:25 +0300

    It compiles. But not runs in debug mode...

--- cmake/finders/Findglib.cmake
+++ cmake/finders/Findglib.cmake
@@ -4,17 +4,20 @@
 endif(GLIB_INCLUDE_DIR)
 
 find_path(GLIB_INCLUDE_DIR glib/glib-init.h)
+find_path(GLIBMM_INCLUDE_DIR glibmm/main.h)
 find_library(GLIB_LIBRARY
     NAMES glib libglib glib-2.0 libglib-2.0)
+find_library(GLIBMM_LIBRARY
+    NAMES glibmm libglibmm glibmm-2.0 libglibmm-2.0)
 # Handle the QUIETLY and REQUIRED arguments and set GLIB_FOUND
 # to TRUE if all listed variables are TRUE.
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(glib DEFAULT_MSG
-    GLIB_INCLUDE_DIR
-    GLIB_LIBRARY)
+    GLIB_INCLUDE_DIR GLIBMM_INCLUDE_DIR
+    GLIB_LIBRARY GLIBMM_LIBRARY)
 
 if(GLIB_FOUND)
-  set(GLIB_LIBRARIES ${GLIB_LIBRARY})
+  set(GLIB_LIBRARIES ${GLIB_LIBRARY} ${GLIBMM_LIBRARY})
 else(GLIB_FOUND)
   set(GLIB_LIBRARIES)
 endif(GLIB_FOUND)
--- thirdparty/cmake-cfg/thirdparty.cmake
+++ thirdparty/cmake-cfg/thirdparty.cmake
@@ -1,8 +1,6 @@
 set(CMAKE_MODULE_PATH ${CMAKE_MODULE_PATH} ${CMAKE_CURRENT_SOURCE_DIR}/cmake/finders)
 
-add_definitions (-D_GLIBCXX_USE_CXX11_ABI=1)
 find_package(gtkmm)
-find_package(glib)
 #find_package(GTK2 REQUIRED gtkmm)
 #find_package(efsw CONFIG REQUIRED)
 
