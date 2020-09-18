0cffa7d0186faece1ad329bb1c88fe498537b12f
Серый Волк <faserg1@users.noreply.github.com>
2020-09-18 16:10:46 +0300

    cairo debug library

--- cmake/finders/Findcairo.cmake
+++ cmake/finders/Findcairo.cmake
@@ -5,11 +5,15 @@ endif(CAIRO_INCLUDE_DIR)
 
 find_path(CAIRO_INCLUDE_DIR cairo/cairo.h)
 
-if ($<CONFIG:Debug>)
-  find_library(CAIRO_LIBRARY NAMES cairod libcairod)
-else()
-  find_library(CAIRO_LIBRARY NAMES cairo libcairo)
-endif()
+find_library(CAIRO_LIBRARY_RELEASE NAMES cairo libcairo)
+find_library(CAIRO_LIBRARY_DEBUG NAMES cairod libcairod)
+
+# И так, я понял как работает это говно.
+# Оно (выражения генератора) будет работать не на этапе `cmake найди мне говно`, а на этапе прямо перед сборкой
+# В результате строку мы имеет вот именно такого дерьмового вида,
+# А компилятору пойдет строка в зависимости от выбранного режима
+set(CAIRO_LIBRARY "$<IF:$<CONFIG:Debug>,${CAIRO_LIBRARY_DEBUG},${CAIRO_LIBRARY_RELEASE}>")
+
 # Handle the QUIETLY and REQUIRED arguments and set CAIRO_FOUND
 # to TRUE if all listed variables are TRUE.
 include(FindPackageHandleStandardArgs)
